7fc2b8e3a0ee0fc63482c0c0c93ea330a0508f35
harry <harry_emmett@outlook.com>
2023-08-28 23:40:03 +0100

    fixed bug

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-const GRIDSIZE = 9;
+const GRIDSIZE = 16;
 let active = false;
 let activeColor = "black";
 
 const div = document.querySelector(".container");
 const colourPicker = document.querySelector(".colour-picker");
+const resetButton = document.querySelector(".reset-button");
+const gridSize = document.querySelector(".grid-size");
 
 const createGrid = (num) => {
   for (let i = 0; i < num; i++) {
     const row = document.createElement("div");
     for (let j = 0; j < num; j++) {
       const square = document.createElement("div");
+      square.className = "square";
       square.style.height = `${350 / num}px`;
       square.style.width = `${350 / num}px`;
       row.appendChild(square);
@@ -20,17 +23,30 @@ const createGrid = (num) => {
 
 createGrid(GRIDSIZE);
 
+const squares = document.querySelectorAll(".square");
+
+// gridSize.addEventListener("click", (e) => {
+//   console.log(e.target.value);
+// }); 
+
 colourPicker.addEventListener("input", (e) => {
     activeColor = e.target.value;
 });
 
+resetButton.addEventListener("click", (e) => {
+  squares.forEach((sq) => {
+    sq.style.backgroundColor = "white";
+  })
+})
+
 div.addEventListener("click", (e) => {
   active = !active;
 
-  div.addEventListener("mouseover", (e) => {
-    if (active) {
-        // works but this is setting the entire backgroud black??
-      e.target.style.backgroundColor = activeColor;
-    }
-  });
+  squares.forEach((sq) => {
+    sq.addEventListener("mouseover", (e) => {
+      if(active) {
+        e.target.style.backgroundColor = activeColor;
+      }
+    })
+  })
 });
